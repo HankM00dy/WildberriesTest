5d54d0fcab066446f2e78a9ccfd1ecaa4582b893
Hank Moody <Skrat_57@mail.ru>
2021-10-07 17:44:58 +0300

    Добавил обертку с гитхаба, проблема с запуском группы и тегом <include> пока не решена

--- src/test/java/frontend/helper/ApplicationManager.java
+++ src/test/java/frontend/helper/ApplicationManager.java
@@ -1,30 +1,24 @@
 package frontend.helper;
 
-import com.codeborne.selenide.Browsers;
-import com.codeborne.selenide.Configuration;
 import frontend.pages.*;
-import org.openqa.selenium.remote.BrowserType;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 
 public class ApplicationManager {
-    public MainPage mainPage = new MainPage();
-    public MenCategoryPage menCategoryPage = new MenCategoryPage();
-    public CatalogPage catalogPage = new CatalogPage();
-    public CartPage cartPage = new CartPage();
-    public CardOfProductPage cardOfProductPage = new CardOfProductPage();
-    public ShoesCategoryPage shoesCategoryPage = new ShoesCategoryPage();
-    public LoginPage loginPage = new LoginPage();
 
-    public void init() {
-        Configuration.browser = Browsers.OPERA;
-        Configuration.startMaximized = true;
-        Configuration.timeout = 5000;
-        open("https://www.wildberries.ru");
-    }
+    public CardOfProductPage cardOfProductPage;
+    public CartPage cartPage;
+    public CatalogPage catalogPage;
+    public LoginPage loginPage;
+    public MainPage mainPage;
+    public MenCategoryPage menCategoryPage;
+    public ShoesCategoryPage shoesCategoryPage;
 
-    public void stop() {
-        closeWebDriver();
+    public ApplicationManager() {
+        cardOfProductPage = PageBuilder.buildCardOfProductPage();
+        cartPage = PageBuilder.buildCartPage();
+        catalogPage = PageBuilder.buildCatalogPage();
+        loginPage = PageBuilder.buildLoginPage();
+        mainPage = PageBuilder.buildMainPage();
+        menCategoryPage = PageBuilder.buildMenCategoryPage();
+        shoesCategoryPage = PageBuilder.buildShoesCategoryPage();
     }
 }
--- src/test/java/frontend/pages/TestBase.java
+++ src/test/java/frontend/pages/TestBase.java
@@ -1,19 +1,33 @@
 package frontend.pages;
 
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
 import frontend.helper.ApplicationManager;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app = new ApplicationManager();
+    protected SoftAssert softAssert;
+//    protected Logger logger;
+
 
     @BeforeMethod
-    public void setUp() {
-        app.init();
+    public void init() {
+        System.setProperty("selenide.browser", "Chrome");
+        Configuration.startMaximized = true;
+        Configuration.timeout = 5000;
+        open("https://www.wildberries.ru");
     }
 
     @AfterMethod
-    public void tearDown() {
-        app.stop();
+    public void stop() {
+        closeWebDriver();
     }
 }
