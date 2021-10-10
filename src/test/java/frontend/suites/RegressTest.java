8cad34900c23390119ba053455822cf737274768
Hank Moody <Skrat_57@mail.ru>
2021-10-10 16:35:49 +0300

    Прикрутил скриншоты для упавших тестов, убрал html-страницу из отчета

--- src/test/java/frontend/suites/RegressTest.java
+++ src/test/java/frontend/suites/RegressTest.java
@@ -37,4 +37,82 @@ public class RegressTest extends TestBase {
                 .assertTextHeadingMensCategoryShouldBeVisible()
                 .clickButtonClothes();
     }
+
+    @Test(description = "Сценарий №4. Регресс тест")
+    public void fourthRegressTest() {
+        app.mainPage
+                .assertInputMainSearchLineShouldBeVisible()
+                .clickButtonNavigation()
+                .clickButtonMensCategory();
+        app.menCategoryPage
+                .assertTextHeadingMensCategoryShouldBeVisible()
+                .clickButtonClothes();
+        app.catalogPage
+                .clickButtonCostumes()
+                .assertImageOfItemShouldBeVisible()
+                .clickCheckboxSportCostume()
+                .assertCheckboxShouldBeSelected("Костюм спортивный")
+                .setInputBrandSearch("adidas")
+                .clickCheckboxBrand("adidas")
+                .assertCheckboxShouldBeSelected("adidas")
+                .clickButtonSortByPrice()
+                .clickImageProductInCatalog(0);
+        app.cardOfProductPage
+                .assertImageOFProductShouldBeVisible()
+                .clickButtonSizeOfProduct("2")
+                .clickButtonAddToCart()
+                .assertButtonGoToCartShouldBeVisible();
+        String nameOfFirstProduct = app.cardOfProductPage.getTextNameOfProduct();
+        app.cardOfProductPage
+                .clickButtonTurnBack();
+        app.catalogPage
+                .clickImageProductInCatalog(2);
+        app.cardOfProductPage
+                .assertImageOFProductShouldBeVisible()
+                .clickButtonSizeOfProduct("2")
+                .clickButtonAddToCart()
+                .assertButtonGoToCartShouldBeVisible();
+        String nameOfSecondProduct = app.cardOfProductPage.getTextNameOfProduct();
+        app.mainPage
+                .clickButtonNavigation()
+                .clickButtonShoesCategory();
+        app.shoesCategoryPage
+                .clickButtonMenShoes();
+        app.catalogPage
+                .clickButtonSneakersSection()
+                .clickCheckboxSneakers()
+                .assertCheckboxShouldBeSelected("Кроссовки")
+                .setInputBrandSearch("adidas")
+                .clickCheckboxBrand("adidas")
+                .assertCheckboxShouldBeSelected("adidas")
+                .clickButtonSortByPrice()
+                .clickImageProductInCatalog(0);
+        app.cardOfProductPage
+                .assertImageOFProductShouldBeVisible()
+                .clickButtonSizeOfProduct("2")
+                .clickButtonAddToCart()
+                .assertButtonGoToCartShouldBeVisible();
+        String nameOfThirdProduct = app.cardOfProductPage.getTextNameOfProduct();
+        app.cardOfProductPage
+                .clickButtonGoToCart();
+        app.cartPage
+                .assertTextNameOfProductShouldBeVisible(nameOfFirstProduct)
+                .assertTextNameOfProductShouldBeVisible(nameOfSecondProduct)
+                .assertTextNameOfProductShouldBeVisible(nameOfThirdProduct)
+                .clickButtonOrder()
+                .clickButtonSelectAddress()
+                .clickButtonSelectAddressInDialog()
+                .clickButtonFirstAddress(0)
+                .clickButtonApplyLocation()
+                .clickButtonPaidByCard()
+                .setInputName("1111111111")
+                .setInputSurname("1111111111")
+                .setInputPhone("1111111111")
+                .clickButtonConfirmOrder()
+                .assertTextErrorIncorrectNameShouldBeVisible()
+                .assertTextErrorIncorrectSurnameShouldBeVisible()
+                .clickButtonEnter();
+        app.loginPage
+                .assertTextHeadingEnterInYourProfileShouldBeVisible();
+    }
 }
